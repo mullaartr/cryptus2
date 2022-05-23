895545dcfcef2713b7d749374bafba61652da004
Lasha Less <lashawilldata@gmail.com>
2022-05-23 11:24:16 +0200

    resolved conflicts

--- src/main/java/com.example.cryptus/controller/LoginController.java
+++ src/main/java/com.example.cryptus/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.example.cryptus.controller;
 
 import com.example.cryptus.model.Account;
+import com.example.cryptus.model.Address;
 import com.example.cryptus.model.Customer;
 import com.example.cryptus.model.User;
 import com.example.cryptus.service.AuthenticatieService;
@@ -27,32 +28,26 @@ public class LoginController {
             "Adam",
             "von",
             "Hilversum",
+            forUser.hashSaltNPepper("password"),
+            "alsomail@email.com",
             Date.valueOf("1982-12-12"),
             "12345678",
-            "In the middle of Nowhere",
-            42,
-            "0908IO",
-            "Babylon",
+            new Address(22, "Suchlaan", "1234DK", "Babylon"),
             "adam@hilversum.von",
-            908070606,
-            "alsomail@email.com",
-            forUser.hashSaltNPepper("password"),
+            "09837374382487",
             "**********");
 
     Customer devil = new Customer(0,
             "Devil",
             "von",
             "Hell",
+            forUser.hashSaltNPepper("idunno"),
+            "wrong@password.com",
             Date.valueOf("1666-12-21"),
             "66666666",
-            "Devil's in the details",
-            42,
-            "0123AH",
-            "Beelzebub",
-            "wrong@username.com",
-            908070606,
-            "wrong@password.com",
-            forUser.hashSaltNPepper("idunno"),
+            new Address(11, "Thislaan", "0102AB", "Hilversum"),
+            "wrong@mail.com",
+            "908070606",
             "**********");
 
     @Autowired
