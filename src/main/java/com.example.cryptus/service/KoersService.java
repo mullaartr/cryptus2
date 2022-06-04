d4d0849da7ff2c10e254cdb47ad988a64728ae9e
sebastiaan klokgieters <sebastiaan.klokgieters@hva.nl>
2022-06-04 15:01:27 +0200

    methode voor koers ophalen gemaakt

--- src/main/java/com.example.cryptus/service/KoersService.java
+++ src/main/java/com.example.cryptus/service/KoersService.java
@@ -1,31 +1,35 @@
 package com.example.cryptus.service;
+import com.example.cryptus.model.Koers;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
-public class KoersService {
+public class KoersService <T>{
 
-    //List<Koers> koersen;
+    List<Koers> koersen;
     private final Logger logger = LogManager.getLogger(KoersService.class);
+
 
     public KoersService() {
         logger.info("new koersService created");
     }
 
-    //@Scheduled(fixedRate = 400000)
-    public void coinAPI() throws IOException {
+    @Scheduled(fixedRate = 400000)
+    public void coinAPI() throws IOException, JSONException {
      URL url = new URL("https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=bitcoin%2Cethereum%2Ctether%2Cusd-coin%2" +
              "Cbinancecoin%2Cripple%2Ccardano%2Cbinance-usd%2Csolana%2Cdogecoin%2Cpolkadot%2Cwrapped-bitcoin%2Ctron%2Clido" +
              "-staked-ether-wormhole%2Cdai%2Cavalanche-2%2Cshiba-inu%2Cleo%2Ccronos-wormhole%2Clitecoin&order=market_cap_desc&per" +
@@ -41,15 +45,12 @@ public class KoersService {
 
      httpURLConnection.setInstanceFollowRedirects(false);
      HttpURLConnection.setFollowRedirects(false);
+     ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+     koersen = mapper.readValue(httpURLConnection.getInputStream(),  new TypeReference<List<Koers>>(){});
 
-     BufferedReader in = new BufferedReader(
-             new InputStreamReader(httpURLConnection.getInputStream()));
-     String inputLine;
-     StringBuffer content = new StringBuffer();
-     while ((inputLine = in.readLine()) != null) {
-         content.append(inputLine);
-     }
-     in.close();
+
+
+
      httpURLConnection.disconnect();
 
 
