6f0557258773df77a7301421abe2af87fa3114dc
Lasha Less <lashawilldata@gmail.com>
2022-06-08 17:24:41 +0200

    token authorization and authentication integrated with DB users

--- src/main/java/com.example.cryptus/service/ApplicationUserService.java
+++ src/main/java/com.example.cryptus/service/ApplicationUserService.java
@@ -1,28 +1,45 @@
 package com.example.cryptus.service;
 
-import com.example.cryptus.dao.ApplicationUserDao;
+import com.example.cryptus.dao.CustomerDao;
+import com.example.cryptus.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import static com.example.cryptus.security.ApplicationUserRole.ADMIN;
+import static com.example.cryptus.security.ApplicationUserRole.CUSTOMER;
+
 @Service
 public class ApplicationUserService implements UserDetailsService {
 
-    private final ApplicationUserDao applicationUserDao;
+//    private final ApplicationUserDao applicationUserDao;
+    private final CustomerDao customerDao;
 
     @Autowired
-    public ApplicationUserService(@Qualifier("fake") ApplicationUserDao applicationUserDao) {
-        this.applicationUserDao = applicationUserDao;
+    public ApplicationUserService(CustomerDao customerDao) {
+        this.customerDao = customerDao;
     }
+
+//    @Autowired
+//    public ApplicationUserService(@Qualifier("fake") ApplicationUserDao applicationUserDao) {
+//        this.applicationUserDao = applicationUserDao;
+//    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return applicationUserDao
-                .selectApplicationUserByUsername(username)
+        User user = customerDao.findCustomerByUsernamePassword(username)
                 .orElseThrow(() -> new UsernameNotFoundException(String
                         .format("Username %s not found", username)));
+        ApplicationUser applicationUser = new ApplicationUser (ADMIN.getGrantedAuthorities(),
+        user.getPassword(),
+                user.getUserName(),
+                true,
+                true,
+                true,
+                true);
+        return applicationUser;
     }
 }
