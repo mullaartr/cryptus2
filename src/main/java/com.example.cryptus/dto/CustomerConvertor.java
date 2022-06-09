8a553d808508aefe0e4aba926fe53895ab01de8d
Khaled Mekky <khaled.mekky@hva.nl>
2022-06-09 12:34:56 +0200

    UnitTest customer repository and adjusted rowmapper with address getters

---
+++ src/main/java/com.example.cryptus/dto/CustomerConvertor.java
@@ -0,0 +1,64 @@
+package com.example.cryptus.dto;
+
+
+import com.example.cryptus.model.Customer;
+import org.springframework.stereotype.Component;
+import java.sql.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Component
+public class CustomerDTOConvertor {
+
+
+
+    public CustomerDTO entityToDTO(Customer customer){
+        CustomerDTO customerDTO = new CustomerDTO();
+
+        customerDTO.setUserId(customer.getUserId());
+        customerDTO.setFirstName(customer.getFirstName());
+        customerDTO.setPreposition(customer.getPreposition());
+        customerDTO.setLastName(customer.getLastName());
+        customerDTO.setBirthDate((Date) customer.getBirthDate());
+        customerDTO.setBSN(customer.getBSN());
+        customerDTO.setEmail(customer.getEmail());
+        customerDTO.setPhone(customer.getPhone());
+        customerDTO.setAddress(customer.getAddress());
+
+        return customerDTO;
+    }
+
+
+    public List<CustomerDTO> entityToDTO( List <Customer> customer){
+
+
+        return customer.stream().map(C -> entityToDTO(C)).collect(Collectors.toList());
+    }
+
+    public Customer dtoToEntity(CustomerDTO customerDTO){
+        Customer customer = new Customer();
+
+        customer.setUserId(customerDTO.getUserId());
+        customer.setFirstName(customerDTO.getFirstName());
+        customer.setPreposition(customerDTO.getPreposition());
+        customer.setLastName(customerDTO.getLastName());
+        customer.setBirthDate(customerDTO.getBirthDate());
+        customer.setBSN(customerDTO.getBSN());
+        customer.setEmail(customerDTO.getEmail());
+        customer.setPhone(customerDTO.getPhone());
+        customer.setAddress(customerDTO.getAddress());
+
+        return customer;
+    }
+
+    public List <Customer> dtoToEntity(List <CustomerDTO> customerDTO){
+
+        return customerDTO.stream().map(c -> dtoToEntity(c)).collect(Collectors.toList());
+    }
+
+
+
+
+
+}
