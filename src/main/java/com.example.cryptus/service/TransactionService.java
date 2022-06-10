b4a086b4922984c8270425923c82561f5855b444
mullaar <rogier.mullaart@hva.nl>
2022-06-10 10:48:39 +0200

    Fix and Refactor buyFromBank methode

--- src/main/java/com.example.cryptus/service/TransactionService.java
+++ src/main/java/com.example.cryptus/service/TransactionService.java
@@ -1,6 +1,8 @@
 package com.example.cryptus.service;
 
-import com.example.cryptus.model.*;
+import com.example.cryptus.model.Asset;
+import com.example.cryptus.model.Customer;
+import com.example.cryptus.model.Transaction;
 import com.example.cryptus.repository.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,16 +19,18 @@ public class TransactionService {
     private CustomerRepository customerRepository;
     private PortefeuilleRepository portefeuilleRepository;
     private BankConfigRepository bankConfigRepository;
-    private BankAccountRepository bankAccountRepository;
+    private BankAccountService bankAccountService;
     private AssetRepository assetRepository;
     private KoersRepository koersRepository;
+    private static final int BANK = 1;
+    private static final double PERCENTAGE = 100.00;
 
 
     public TransactionService(TransactionRepository transactionRepository,
                               CustomerRepository customerRepository,
                               PortefeuilleRepository portefeuilleRepository,
                               BankConfigRepository bankConfigRepository,
-                              BankAccountRepository bankAccountRepository,
+                              BankAccountService bankAccountService,
                               AssetRepository assetRepository,
                               KoersRepository koersRepository) {
         super();
@@ -34,157 +38,81 @@ public class TransactionService {
         this.customerRepository = customerRepository;
         this.portefeuilleRepository = portefeuilleRepository;
         this.bankConfigRepository = bankConfigRepository;
-        this.bankAccountRepository = bankAccountRepository;
+        this.bankAccountService = bankAccountService;
         this.assetRepository = assetRepository;
         this.koersRepository = koersRepository;
 
         logger.info("Nieuwe transactieservice");
     }
-
     public List<Transaction> getBuyTransactionsFromUser(int userId) {
         return transactionRepository.getBuyTransactionsFromUser(userId);
     }
-
     public List<Transaction> getSellTransactionsFromUser(int userId) {
         return transactionRepository.getSellTransactionsFromUser(userId);
     }
-
     public Optional<Transaction> buyFromBank(Customer buyer, String assetNaam, double assetAmount) {
 
-        //A. ASSETGEDEELTE
-
-        //1. de koper is bekend en krijg ik binnen via de controller
-
-        //2. seller = bank = userId 1
-
-        Optional<Customer> seller = customerRepository.findCustomerById(1);
-
-        //3. Ophalen portefeuille van de buyer en seller:
-
-        Portefeuille portefeuilleBuyer =
-                portefeuilleRepository.findPortefeuilleWithCustomerById(buyer.getUserId()).get();
-
-        Optional<Portefeuille> portefeuilleSeller =
-                portefeuilleRepository.findPortefeuilleWithCustomerById(seller.get().getUserId());
-
-        //4.check of de bank de asset überhaupt heeft en of er voldoende
-        // saldo is. Zo ja (true) ga dan door.
-
-        if (portefeuilleSeller.get().hasEnoughAssets(assetNaam,
-                assetAmount)) {
-
-            // 5.Schrijf het amount van assets af van de portefeuille van de
-            // seller
-
-            Asset assetSeller =
-                    portefeuilleSeller.get().getAssetLijst().stream().filter(asset1 -> asset1.getAssetNaam().equals(assetNaam)).findAny().orElse(null);
-            assetSeller.setSaldo(assetSeller.getSaldo() - assetAmount);
-
-            portefeuilleRepository.updatePortefeuille(portefeuilleSeller.get(), assetSeller);
-
-            // 6. Schrijf de amount van assets bij bij de portefeuille van de
-            // koper
-
-            Asset assetBuyer =
-                    portefeuilleBuyer.getAssetLijst().stream().filter(asset1 -> asset1.getAssetNaam().equals(assetNaam)).findAny().orElse(null);
-            assetBuyer.setSaldo(assetBuyer.getSaldo() + assetAmount);
-
-            portefeuilleRepository.updatePortefeuille(portefeuilleBuyer,
-                    assetBuyer);
-        }
-
-        //B. BEPALEN WAARDE EN COMMISSIE = TOTALE WAARDE
-
-        // 8. Waarde van de aankoop in euro's.
-
+        Optional<Customer> seller = customerRepository.findCustomerById(BANK);
+        Optional<Asset> assetBought = assetRepository.findAssetByAssetNaam(assetNaam);
         double valueTransaction = calcValueTransactionInEuro(assetNaam,
                 assetAmount);
-
-        // 9. Waarde van de commissie. Zie ook methode onder
         double valueFee = calcFee(valueTransaction);
-
-        // 10. Totale kosten. Optellen kosten aankoop + kosten commissie
-
         double totalValue = calTotal(valueTransaction, valueFee);
-
-        //C. AFSCHRIJVEN EN BIJSCHRIJVEN EURO'S OP BANKREKENING ETC.
-
-        //11. Ophalen bankaccount van de buyer:
-
-        Optional<BankAccount> bankAccountBuyer =
-                bankAccountRepository.findBankaccountByUserId(buyer.getUserId());
-
-        //12. Ophalen bankaccount van de seller (= bank = userId1)
-
-        Optional<BankAccount> bankAccountSeller =
-                bankAccountRepository.findBankaccountByUserId(1);
-
-        //13. check of de buyer voldoende euro's heeft
-        if (bankAccountBuyer.get().hasEnoughBalance(totalValue)) {
-
-            //14. Schrijf het aantal euro's af van de bankrekening van de buyer
-
-            bankAccountBuyer.get().withdrawFunds(totalValue);
-
-            //15. schrijf euro's af en bij de bank
-
-            bankAccountSeller.get().addFunds(totalValue);
-        }
-        //D. Transactie gedeelte
-
-        //16. Haal de Asset op doormiddel van de assetnaam -> hoe moet dit?
-        // -> Volgens mij bestaat deze methode niet in de repository en ook
-        // niet op naam. Wel op basis van ID.
-
-        Optional<Asset> assetBought = assetRepository.findAssetByAssetNaam(assetNaam);
-
-        //17. Haal het huidige percentage uit de DB:
-
         double percentage = bankConfigRepository.getPercentage();
+        if (seller.get().getPortefeuille().hasEnoughAssets(assetNaam, assetAmount)) {
+            addAndWithdrawAssets(buyer, assetNaam, assetAmount, seller);
+        }
+        if (buyer.getBankAccount().hasSufficientFunds(totalValue)) {
 
-        //.18.Maak een transactie met alle ingredienten (Asset mis nog!)
-
-        Transaction transaction = new Transaction(buyer, seller.get(),
-                assetBought.get(),
-                assetAmount, totalValue, percentage);
-
-        //19. Maak een transactie aan in de database
-
-        transactionRepository.createTransaction(transaction);
+            addAndWithdrawEuros(buyer, seller, totalValue);
+        }
+        Transaction transaction = createNewTransaction(buyer, assetAmount, seller, assetBought, totalValue, percentage);
 
         return Optional.of(transaction);
     }
-
-// Haal de koers van het Asset op en vermenigvuldig deze met de hoeveelheid
-// assets
+    private Transaction createNewTransaction(Customer buyer, double assetAmount, Optional<Customer> seller, Optional<Asset> assetBought, double totalValue, double percentage) {
+        Transaction transaction = new Transaction(buyer, seller.get(),
+                assetBought.get(),
+                assetAmount, totalValue, percentage);
+        transactionRepository.createTransaction(transaction);
+        return transaction;
+    }
+    private void addAndWithdrawEuros(Customer buyer, Optional<Customer> seller, double totalValue) {
+        bankAccountService.withdrawFunds(totalValue, buyer.getUserId());
+        bankAccountService.addFunds(totalValue, seller.get().getUserId());
+    }
+    private void addAndWithdrawAssets(Customer buyer, String assetNaam, double assetAmount, Optional<Customer> seller) {
+        Asset assetSeller =
+                seller.get().getPortefeuille().getAssetLijst().stream().filter(asset1 -> asset1.getAssetNaam().equals(assetNaam)).findAny().orElse(null);
+        assetSeller.setSaldo(assetSeller.getSaldo() - assetAmount);
+        portefeuilleRepository.updatePortefeuille(seller.get().getPortefeuille(),
+                assetSeller);
+        Asset assetBuyer =
+                buyer.getPortefeuille().getAssetLijst().stream().filter(asset1 -> asset1.getAssetNaam().equals(assetNaam)).findAny().orElse(null);
+        assetBuyer.setSaldo(assetBuyer.getSaldo() + assetAmount);
+        portefeuilleRepository.updatePortefeuille(buyer.getPortefeuille(),
+                assetBuyer);
+    }
     public double calcValueTransactionInEuro(String assetNaam,double assetAmount) {
         double koersAsset = koersRepository.findKoersByAssetNaam(assetNaam).get().getKoersInEuro();
         System.out.println(koersAsset);
         return koersAsset*assetAmount;
-
     }
-
     public double calcFee(double valueInEuro) {
         double percentage = bankConfigRepository.getPercentage();
-        return valueInEuro * (percentage / 100);
+        return valueInEuro * (percentage / PERCENTAGE);
     }
-
     public double calTotal(double valueInEuro, double feeInEuro) {
         return valueInEuro + feeInEuro;
     }
-
     public Optional<Transaction> updateTransaction(Transaction transaction, int transactionId) {
         transactionRepository.updateTransaction(transaction, transactionId);
         return null;
     }
-
     public void deleteTransaction(int transactionId) {
         transactionRepository.deleteTransaction(transactionId);
     }
-
     public Optional<Transaction> findTransactionById(int transactionId) {
         return transactionRepository.findTransactionById(transactionId);
     }
-
-
 }
