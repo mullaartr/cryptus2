74a79e0028b8fe8d1c978c57983b7b7db71e9a08
sebastiaan klokgieters <sebastiaan.klokgieters@hva.nl>
2022-06-14 20:19:32 +0200

    coinGecko aangepast

--- src/main/resources/static/js/BaseTemplate.js
+++ src/main/resources/static/js/BaseTemplate.js
@@ -2,10 +2,16 @@ koersOphaler()
 
 
 let euroOfDollar = true;
+let $tickerWrapper;
+let $list;
+let $clonedList;
+let listWidth;
 
 document.querySelector('.tickerwrapper').addEventListener("click", () => {
     euroOfDollar = !euroOfDollar;
     koersen.forEach(koersen => vullLijstEuro(koersen));
+    $tickerWrapper.clear();
+
 })
 
 
@@ -50,16 +56,16 @@ function vullLijstEuro(json){
 
 
 function maakTekstLint() {
-    var $tickerWrapper = $(".tickerwrapper");
-    var $list = $tickerWrapper.find("ul.list");
-    var $clonedList = $list.clone();
-    var listWidth = 20;
+     $tickerWrapper = $(".tickerwrapper");
+    $list = $tickerWrapper.find("ul.list");
+    $clonedList = $list.clone();
+    listWidth = 20;
 
     $list.find("li").each(function (i) {
         listWidth += $(this, i).outerWidth(true);
     });
 
-    var endPos = $tickerWrapper.width() - listWidth;
+    let endPos = $tickerWrapper.width() - listWidth;
 
     $list.add($clonedList).css({
         "width": listWidth + "px"
@@ -67,8 +73,8 @@ function maakTekstLint() {
 
     $clonedList.addClass("cloned").appendTo($tickerWrapper);
 
-    var infinite = new TimelineMax({repeat: -1, paused: true});
-    var time = 40;
+    let infinite = new TimelineMax({repeat: -1, paused: true});
+    let time = 40;
 
     infinite.fromTo($list, time, {rotation: 0.01, x: 0}, {force3D: true, x: -listWidth, ease: Linear.easeNone}, 0)
         .fromTo($clonedList, time, {rotation: 0.01, x: listWidth}, {force3D: true, x: 0, ease: Linear.easeNone}, 0)
@@ -77,6 +83,8 @@ function maakTekstLint() {
         .to($list, time, {force3D: true, rotation: 0.01, x: 0, ease: Linear.easeNone}, time)
         .progress(1).progress(1)
         .play();
+
+
     $tickerWrapper.on("mouseenter", function(){
         infinite.pause();
     }).on("mouseleave", function(){
